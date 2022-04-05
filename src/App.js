54498e71f4a21066a6325270da215ec06f55feac
Anthony Suarez <antsuarez305@gmail.com>
2022-04-05 18:56:41 -0500

    Created Single Listing Page

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
 import CreateListing from './pages/CreateListing'
-// import Listing from './pages/Listing'
+import Listing from './pages/Listing'
 
 function App() {
   return (
@@ -28,10 +28,10 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
-          {/* <Route
+          <Route
             path='/category/:categoryName/:listingId'
             element={<Listing />}
-          /> */}
+          />
         </Routes>
         <Navbar />
       </Router>
