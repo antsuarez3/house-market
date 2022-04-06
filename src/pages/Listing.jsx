3005f7a5cd59e403b768a7fd7fe4be631ce45893
Anthony Suarez <antsuarez305@gmail.com>
2022-04-06 18:19:03 -0500

    Created Contact page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
+// import { Helmet } from 'react-helmet'
+// import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+// import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+// import { Swiper, SwiperSlide } from 'swiper/react'
+// import 'swiper/swiper-bundle.css'
 import { getDoc, doc } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { db } from '../firebase.config'
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
+// SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Listing() {
   const [listing, setListing] = useState(null)
@@ -35,7 +41,22 @@ function Listing() {
 
   return (
     <main>
-      {/* SLIDER */}
+      {/* <Helmet>
+        <title>{listing.name}</title>
+      </Helmet>
+      <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: 'cover',
+              }}
+              className='swiperSlideDiv'
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper> */}
 
       <div
         className='shareIconDiv'
@@ -72,6 +93,7 @@ function Listing() {
             $ {listing.regularPrice - listing.discountedPrice} discount
           </p>
         )}
+
         <ul className='listingDetailsList'>
           <li>
             {listing.bedrooms > 1
@@ -86,13 +108,32 @@ function Listing() {
           <li>{listing.parking && 'Parking Spot'}</li>
           <li>{listing.furnished && 'Furnished'}</li>
         </ul>
+
         <p className='listingLocationTitle'>Location</p>
 
-        {/* Map */}
+        {/* <div className='leafletContainer'>
+          <MapContainer
+            style={{ height: '100%', width: '100%' }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+            />
+
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
+        </div> */}
 
         {auth.currentUser?.uid !== listing.userRef && (
           <Link
-            to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`}
+            to={`/contact/${listing.userRef}?listingName=${listing.name}`}
             className='primaryButton'
           >
             Contact Landlord
